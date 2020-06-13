b6979a6125eaa1c4b989546e2ed088feeb03811f
Jae <jae@zero5.co>
2020-06-13 18:02:21 +0900

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-sarah"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ldfcdpruw9.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_aeKfOxfVz",
+    APP_CLIENT_ID: "1e0ilenjcl18hipfq57crpibhc",
+    IDENTITY_POOL_ID: "us-east-2:3c6b2754-62b7-468d-b41b-af8a56cbec5a"
+  }
+};
